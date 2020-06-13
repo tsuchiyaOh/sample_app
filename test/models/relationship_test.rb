a64e98993ebbeecf24f90f40a3eada5c1a22dd0b
tsuchiyaOh <ohoh15977@gmail.com>
2020-06-13 04:19:35 +0000

    Add user following

---
+++ test/models/relationship_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class RelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id, followed_id: users(:archer).id)
+  end
+end
